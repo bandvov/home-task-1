483b8822a84d7a493cb761111026df220d303bf5
Iryna Hrynchuk <45430954+chak-kit@users.noreply.github.com>
2020-02-06 00:08:49 +0200

    add Map and class for execution of any task

--- script.js
+++ script.js
@@ -4,14 +4,15 @@ function swapMarginalDigits(num) {
   let result = numToStr.slice(-1) + numToStr.slice(1, -1) + numToStr.slice(0, 1);
   console.log(parseInt(result))
 }
-swapMarginalDigits(12345);
+// swapMarginalDigits(12345);
 
 // 88г
 function wrapByOne(num) {
   let result = '1'.concat(num, '1');
   console.log(parseInt(result))
 }
-wrapByOne(555);
+// wrapByOne(555);
+
 
 // 332
 function findLagrangeNumbers(num) {
@@ -29,4 +30,35 @@ function findLagrangeNumbers(num) {
     }
   }
 }
-findLagrangeNumbers(50);
+// findLagrangeNumbers(50);
+
+
+
+class Task {
+  constructor(description, func) {
+    this.description = description;
+    this.func = func;
+  }
+
+  run() {
+    console.log(this.description);
+    this.func();
+  }
+}
+
+const tasks = new Map();
+tasks.set('88v', new Task("Swap the first and last digits of number n", () => swapMarginalDigits(12345)));
+tasks.set('88h', new Task("Add one at the beginning and the end of the number n", () => wrapByOne(555)));
+tasks.set('332', new Task("It is known that any natural number can be represented as a sum of not more than four " +
+  "squares of natural numbers or, which is the same as the sum of four squares of non-negative integers (Langrange's " +
+  "theorem). Given a natural number n. Find the non-negative: x, y, z, t, where n = x*x + y*y +z*z + t*t",
+  () => findLagrangeNumbers(50)));
+
+let number = prompt('Enter task number:');
+task = tasks.get(number);
+
+if(task) {
+  task.run();
+} else {
+  console.log("Task not found")
+}
