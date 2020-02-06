2aa8e75fe8f2b2b3fc68ee8e1cf525b5681f8801
Iryna Hrynchuk <45430954+chak-kit@users.noreply.github.com>
2020-02-06 21:39:33 +0200

    refactoring

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-// 88в
+// 88v
 function swapMarginalDigits(num) {
-  let numToStr = num.toString();
-  let result = numToStr.slice(-1) + numToStr.slice(1, -1) + numToStr.slice(0, 1);
-  console.log(parseInt(result))
+  const result = num.slice(-1) + num.slice(1, -1) + num.slice(0, 1);
+  console.log(parseInt(result));
 }
-// swapMarginalDigits(12345);
 
-// 88г
+// 88h
 function wrapByOne(num) {
-  let result = '1'.concat(num, '1');
-  console.log(parseInt(result))
+  const result = '1'.concat(num, '1');
+  console.log(parseInt(result));
 }
-// wrapByOne(555);
-
 
 // 332
-function findLagrangeNumbers(num) {
+function findLagrangeNumbers(numStr) {
+  let num = +numStr;
   let sqrtOfnum = Math.floor(Math.sqrt(num));
   for (let x = 1; x <= sqrtOfnum; x++) {
     for (let y = 1; y <= sqrtOfnum; y++) {
@@ -23,14 +20,14 @@ function findLagrangeNumbers(num) {
         for (let t = 1; t <= sqrtOfnum; t++) {
           if ((x * x + y * y + z * z + t * t) === num) {
             console.log(`x= ${x}, y= ${y}, z= ${z}, t= ${t}`);
-            return;
+            return
           }
         }
       }
     }
   }
+  console.log('This numbers were not found');
 }
-// findLagrangeNumbers(50);
 
 
 
@@ -41,24 +38,41 @@ class Task {
   }
 
   run() {
-    console.log(this.description);
-    this.func();
+    let params = prompt(this.description);
+    this.func(params);
   }
 }
 
 const tasks = new Map();
-tasks.set('88v', new Task("Swap the first and last digits of number n", () => swapMarginalDigits(12345)));
-tasks.set('88h', new Task("Add one at the beginning and the end of the number n", () => wrapByOne(555)));
-tasks.set('332', new Task("It is known that any natural number can be represented as a sum of not more than four " +
-  "squares of natural numbers or, which is the same as the sum of four squares of non-negative integers (Langrange's " +
-  "theorem). Given a natural number n. Find the non-negative: x, y, z, t, where n = x*x + y*y +z*z + t*t",
-  () => findLagrangeNumbers(50)));
+
+tasks.set(
+  '88v',
+  new Task('Swap the first and last digits of number n. Enter n:', (params) =>
+    swapMarginalDigits(...params.split(","))
+  )
+);
+
+tasks.set(
+  '88h',
+  new Task('Add one at the beginning and the end of the number n. Enter n:', (params) =>
+    wrapByOne(...params.split(","))
+  ),
+);
+
+tasks.set(
+  '332',
+  new Task(
+    "It is known that any natural number can be represented as a sum of not more than four " +
+    "squares of natural numbers or, which is the same as the sum of four squares of non-negative integers (Langrange's " +
+    "theorem). Given a natural number n. Find the non-negative: x, y, z, t, where n = x*x + y*y +z*z + t*t. Enter n:",
+    (params) => findLagrangeNumbers(...params.split(",")))
+);
 
 let number = prompt('Enter task number:');
 task = tasks.get(number);
 
-if(task) {
+if (task) {
   task.run();
 } else {
-  console.log("Task not found")
+  console.log('Task not found');
 }
