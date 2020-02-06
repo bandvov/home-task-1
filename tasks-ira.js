8b9e8531bf765cb5f61aeefddb946e083ecd32f1
Iryna Hrynchuk <45430954+chak-kit@users.noreply.github.com>
2020-02-06 23:27:11 +0200

    Update tasks-ira.js

--- tasks-ira.js
+++ tasks-ira.js
@@ -4,14 +4,14 @@ import {tasks} from './constructor.js'
 tasks.set(
   '88v',
   new Task('Swap the first and last digits of number n. Enter n:', (params) =>
-    swapMarginalDigits(...params.split(","))
+    swapMarginalDigits(...params)
   )
 );
 
 tasks.set(
   '88h',
   new Task('Add one at the beginning and the end of the number n. Enter n:', (params) =>
-    wrapByOne(...params.split(","))
+    wrapByOne(...params)
   ),
 );
 
@@ -21,12 +21,13 @@ tasks.set(
     "It is known that any natural number can be represented as a sum of not more than four " +
     "squares of natural numbers or, which is the same as the sum of four squares of non-negative integers (Langrange's " +
     "theorem). Given a natural number n. Find the non-negative: x, y, z, t, where n = x*x + y*y +z*z + t*t. Enter n:",
-    (params) => findLagrangeNumbers(...params.split(",")))
+    (params) => findLagrangeNumbers(...params))
 );
 
 // 88v
 function swapMarginalDigits(num) {
-  const result = num.slice(-1) + num.slice(1, -1) + num.slice(0, 1);
+  let numStr = num.toString();
+  const result = numStr.slice(-1) + numStr.slice(1, -1) + numStr.slice(0, 1);
   console.log(parseInt(result));
 }
 
@@ -37,8 +38,7 @@ function wrapByOne(num) {
 }
 
 // 332
-function findLagrangeNumbers(numStr) {
-  let num = +numStr;
+function findLagrangeNumbers(num) {
   let sqrtOfnum = Math.floor(Math.sqrt(num));
   for (let x = 1; x <= sqrtOfnum; x++) {
     for (let y = 1; y <= sqrtOfnum; y++) {
